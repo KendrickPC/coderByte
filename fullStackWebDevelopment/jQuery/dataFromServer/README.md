ae37e3ddbd91297b82e67579b2ffaecf0ccab4a9
kendrickpc <kennethpchang@gmail.com>
2019-02-19 15:35:57 +0800

    README.md for AJAX notes added.

---
+++ fullStackWebDevelopment/jQuery/dataFromServer/README.md
@@ -0,0 +1,8 @@
+# Below are a few helpful resources regarding AJAX:
+
+##### AJAX tutorial - jQuery
+[AJAX | jQuery link](https://learn.jquery.com/ajax/)
+#####
+#####
+#####
+#####
--- fullStackWebDevelopment/jQuery/dataFromServer/scripts.js
+++ fullStackWebDevelopment/jQuery/dataFromServer/scripts.js
@@ -1,19 +1,22 @@
 // coderbyte.com/api/test/data/json
 
 $(document).ready(function() {
+  $('#click').on('click', function() {
 
     $.ajax({
-        type: 'GET',
-        url: 'https://coderbyte.com/api/test/data/json',
-        dataType: 'jsonp',
-        jsonpCallback: 'func',
-        success: function(data) {
-          for (var i =0; i < data.people.length; i++) {
-            $('#people').append('<li>' + data.people[i].name + '</li>');
-          }
-        },
-        error: function() {
-          console.log('error');
+      type: 'GET',
+      url: 'https://coderbyte.com/api/test/data/json',
+      dataType: 'jsonp',
+      jsonpCallback: 'func',
+      success: function(data) {
+        console.log(data);
+        for (var i =0; i < data.people.length; i++) {
+          $('#people').append('<li>' + data.people[i].name + '</li>');
         }
+      },
+      error: function() {
+        console.log('error');
+      }
     });
-});  
+  });
+});
