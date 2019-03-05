2a28f590783da449178cd6f8170a73babd546f0b
kendrickpc <kennethpchang@gmail.com>
2019-03-05 13:08:34 +0800

    Power set problem completed.

--- fullStackWebDevelopment/interviewPrep/4PowerSetOfGivenSet/powerSet.js
+++ fullStackWebDevelopment/interviewPrep/4PowerSetOfGivenSet/powerSet.js
@@ -1,1 +1,26 @@
-powerSet.js
+function powerSet(arr) {
+  // The final power set.
+  var powers = [];
+  // The total number of sets that the power set will contain.
+  var total = Math.pow(2, arr.length);
+
+  // Looping through each value from 0 to 2^n.
+  for (var i = 0; i < total; i++) {
+    // A set added to the power set.
+    var temporarySet = [];
+    // Converting the integer to binary code.
+    var num = i.toString(2);
+    // Pad the binary number so 1 becomes 001.
+    while (num.length < arr.length) { num = '0' + num; }
+    // Build the set that matches the 1's in the binary number.
+    for (var b = 0; b < num.length; b++) {
+      if (num[b] === '1') { temporarySet.push(arr[b]); }
+    }
+    // Add this set to the final power set.
+    powers.push(temporarySet);
+  }
+  return powers;
+}
+
+// powerSet(['Jim', 'John', 'Joe']);
+powerSet([1, 2, 3]);
