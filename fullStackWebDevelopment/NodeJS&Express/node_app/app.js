53a10b90559f1cdaf6434613e5c11f5386abe106
kendrickpc <kennethpchang@gmail.com>
2019-03-11 18:11:27 +0800

    node app.js file user profiles working after server reset.

--- fullStackWebDevelopment/NodeJS&Express/node_app/app.js
+++ fullStackWebDevelopment/NodeJS&Express/node_app/app.js
@@ -1,13 +1,39 @@
 var express = require('express');
 var app = express();
 
+// you can make a request to a database here and retrieve some data
+// but for this example, we are using a static object of user information
+var Users = {
+
+  'David': {
+    age: 52,
+    occupation: 'Professor',
+    hobby: 'Swimming'
+  },
+
+  'Robert': {
+    age: 34,
+    occupation: 'Engineer',
+    hobby: 'Running'
+  },
+
+  'Jane': {
+    age: 28,
+    occupation: 'Nurse',
+    hobby: 'Chess'
+  }
+
+};
+
 // route pages
 app.get('/', function (req, res) {
   res.send('Main page is up and running!');
 });
 
 app.get('/user', function (req, res) {
-  res.send('TODO: update this page...');
+  var check = Users[req.query.name];
+  var info = (check) ? check : 'User does not exist...';
+  res.send(info);
 });
 
 // what port to run server on
